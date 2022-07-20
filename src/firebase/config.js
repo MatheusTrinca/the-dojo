b853e8963b7a346e5193005d19efe3d044743ee1
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-07-20 17:54:16 -0300

    first

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyANVbP7SPMV0ci7reoJfDFAbDYfaEbb6M4',
+  authDomain: 'the-dojo-ce086.firebaseapp.com',
+  projectId: 'the-dojo-ce086',
+  storageBucket: 'the-dojo-ce086.appspot.com',
+  messagingSenderId: '260642303829',
+  appId: '1:260642303829:web:e0fc75287431951f9809dc',
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth();
+
+export { db, auth };
